41f162bc59b4d2d1e34c93c32d855ea8a0e3441e
e-gagarin <e.gagarincs@gmail.com>
2025-09-24 00:15:18 -0400

    Update Roll_D6.py

--- Roll_D6.py
+++ Roll_D6.py
@@ -7,4 +7,6 @@ def roll_six_sided_die():
 result = roll_six_sided_die()
 print(f"You rolled a {result}!")
       
-#Receiving a syntax error on line 8 that parantheses weren't closed - I had it as (f"You rolled a {result})!", totally valid error
+# Receiving a syntax error on line 8 that parantheses weren't closed - I had it as (f"You rolled a {result})!", totally valid error
+# Code works but dice isn't being rolled at random, rolling 1-4 in order.
+# Disregard previous comment, results are definitely randomized it just so happens that it rolled in sequence, 1 in 1,296 odds of that happening
