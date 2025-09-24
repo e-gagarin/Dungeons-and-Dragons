ebb3baaa922f7932bc0a2f8c718b65e41e933258
e-gagarin <e.gagarincs@gmail.com>
2025-09-24 00:26:15 -0400

    Update DnD_Roll_Die.py

--- DnD_Roll_Die.py
+++ DnD_Roll_Die.py
@@ -1,9 +1,10 @@
 import random
+
 def roll_die(sides):
   """Return a random integer from 1 to 'sides' (inclusive)."""
-#Input validation
-if not isinstance(sides, int) or sides < 1:
-  raise ValueError("sides must be a positive integer")
+  #Input validation
+  if not isinstance(sides, int) or sides < 1:
+    raise ValueError("sides must be a positive integer")
 
 #Do the roll
 return random.randint(1, sides)
