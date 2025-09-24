372516bf4e98657a7c5400dc4bd2479c18725eaf
e-gagarin <e.gagarincs@gmail.com>
2025-09-24 00:28:52 -0400

    Update DnD_Roll_Die.py INDENTATION

--- DnD_Roll_Die.py
+++ DnD_Roll_Die.py
@@ -6,5 +6,5 @@ def roll_die(sides):
   if not isinstance(sides, int) or sides < 1:
     raise ValueError("sides must be a positive integer")
 
-#Do the roll
-return random.randint(1, sides)
+  #Do the roll
+  return random.randint(1, sides)
