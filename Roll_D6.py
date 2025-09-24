5aaddab92c557dcc2f26cda9dd0bdba8a77d0af5
e-gagarin <e.gagarincs@gmail.com>
2025-09-23 23:43:17 -0400

    Create Roll_D6.py

---
+++ Roll_D6.py
@@ -0,0 +1,9 @@
+import random
+
+def roll_six_sided_die();
+"""Sims rolling a six sided die and returns result"""
+return random.randint(1, 6)
+
+result = roll_six_sided_die()
+print("You rolled a {result}!"
+      
