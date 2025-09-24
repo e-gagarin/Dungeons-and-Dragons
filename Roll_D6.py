ddffa9d0c3288ecf1dbaa7fef17ebabd52e28ce4
e-gagarin <e.gagarincs@gmail.com>
2025-09-24 00:04:59 -0400

    Update Roll_D6.py

--- Roll_D6.py
+++ Roll_D6.py
@@ -5,5 +5,6 @@ def roll_six_sided_die():
       return random.randint(1, 6)
 
 result = roll_six_sided_die()
-print(f"You rolled a {result})!"
+print(f"You rolled a {result}!")
       
+#Receiving a syntax error on line 8 that parantheses weren't closed - I had it as (f"You rolled a {result})!", totally valid error
